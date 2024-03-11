a5a7f6bd27b882edc38311de72f8e832e15683a2
Yunus Bagriyanik <yunusbgrynk@gmail.com>
2024-03-12 02:32:11 +0300

    langchain vector document search

---
+++ README.md
@@ -0,0 +1,12 @@
+## Langchain Vector Document Search
+
+1. Install the required dependencies:
+    ```bash
+    pip install -r requirements.txt
+    ```
+2. Set Up Keys
+
+    ```
+    OPENAI_API_KEY=your_openai_api_key
+    PINECONE_API_KEY=your_pinecone_api_key
+    ```
---
+++ main.py
@@ -0,0 +1,39 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+from langchain.chains import RetrievalQA
+from langchain import OpenAI
+import pinecone
+
+load_dotenv()
+
+if __name__ == "__main__":
+    print("Langchain Vector Document Search")
+    loader = TextLoader("FILE_PATH_REFERENCE")
+
+    pinecone_instance = pinecone.Pinecone(api_key=os.environ.get("PINECONE_API_KEY"))
+    print(pinecone_instance.list_indexes())
+
+    document = loader.load()
+
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(len(texts))
+
+    embeddings = OpenAIEmbeddings(openai_api_key=os.environ["OPENAI_API_KEY"])
+    document_search = Pinecone.from_documents(
+        texts, embeddings, index_name="pine_cone_index_name"
+    )
+
+    # return_source_documents= True
+    qa_retrieval = RetrievalQA.from_chain_type(
+        llm=OpenAI(), chain_type="stuff", retriever=document_search.as_retriever()
+    )
+
+    query = "Query"
+    result = qa_retrieval({"query": query})
+    print(result)
